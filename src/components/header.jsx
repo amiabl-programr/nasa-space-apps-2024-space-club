ffc8be8b0062a191e13de3a54c1935ad756bac56
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2024-10-05 15:05:43 +0100

    add next Link and Image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,26 @@
+import Link from "next/link"; 
+import Image from "next/image";
+
 const Header =()=>{
 return (
 <>
     <header className='flex justify-between items-center py-3 px-4 bg-gray-100'>
         <div className='flex items-center'>
             {/* Replace with your logo */}
-            <img alt="Logo" className="w-12 h-12 mr-2" src="" />
+            <Image alt="Logo" className="w-12 h-12 mr-2" src="" />
             <span className="text-xl font-semibold">FarmYield</span>
         </div>
 
         <nav className='space-x-4'>
-            <a className="text-gray-700 hover:text-blue-500" href="/precipitation">
-            Precipitation
-            </a>
-            <a className="text-gray-700 hover:text-blue-500" href="/soil-moisture">
-            Soil Moisture
-            </a>
-            <a className="text-white bg-blue-500 py-2 px-4 rounded-lg hover:bg-blue-600" href="/soil-assessment">
-            Start Soil Assessment
-            </a>
+            <Link  className="text-gray-700 hover:text-blue-500" href="/precipitation">
+                Precipitation
+            </Link>
+            <Link className="text-gray-700 hover:text-blue-500" href="/soil-moisture">
+                Soil Moisture
+            </Link>
+            <Link className="text-white bg-blue-500 py-2 px-4 rounded-lg hover:bg-blue-600" href="/soil-assessment">
+                Start Soil Assessment
+            </Link>
         </nav>
     </header>
 </>
