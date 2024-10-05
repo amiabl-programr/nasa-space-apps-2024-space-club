5969f3d9fda65d7c0eba93c11e2218c0b1f4d742
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2024-10-05 14:18:39 +0100

    add new pages

---
+++ src/pages/precipitation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Precipitation = () => {
+
+    return (
+        <>
+            <div>Hello</div>
+        </>
+    );
+};
+
+export default Precipitation;
---
+++ src/pages/temperature.jsx
@@ -0,0 +1,9 @@
+const Temperature = () => {
+    return (
+        <>
+            <div>Hello</div>
+        </>
+    );
+};
+
+export default Temperature;
